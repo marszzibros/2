885c5b9fb8dd4c2433951be6be1e142b44a7a126
marszzibros <hwasungishungry@gmail.com>
2020-12-29 16:35:36 -0500

    master

--- Initialize.java
+++ Initialize.java
@@ -2,7 +2,6 @@ public class Initialize {
     public static void main(String[] args)
     {
         int month = 2, days = 28;
-        System.out.println("Month " + month + " has " + days + " days.");
-        
+        System.out.println("Month " + month + " has " + days + " days.");  
     }
 }
---
+++ Stringdemo.java
@@ -0,0 +1,10 @@
+public class Stringdemo
+{
+    public static void main(String[] args)
+    {
+        String greeting = "Good morning, ";
+        String name = "Herman";
+        
+        System.out.println(greeting + name);
+    }
+}
